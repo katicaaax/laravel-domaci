158b6b5e94148cbd42ca46f816801163a28f22ea
katicaaax <radosavljevic.katarina98@gmail.com>
2022-11-13 17:00:25 +0100

    Drugi commit. Pravljenje migracija.

--- database/migrations/2022_11_13_155653_create_services_table.php
+++ database/migrations/2022_11_13_155653_create_services_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('store_type', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('store_type');
+        Schema::dropIfExists('services');
     }
 };
